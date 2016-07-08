ef23ed50c043a0a1226eb05ebb11091b957804e9
Sang Theresa Lee <tsahlee@gmail.com>
2016-07-08 00:52:41 -0400

    IIFE, changing background at win.

--- guessingGame.js
+++ guessingGame.js
@@ -1,7 +1,8 @@
 // i thought that everything is supposed to be wrapped around $(document).ready for best practice for jQuery. is this not true?
 /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
-var playersGuess;
+(function(){
+	var playersGuess;
 
 // What is the point of this? Why not just
 // var winningNumber = = Math.floor(Math.random() * 100 + 1);
@@ -27,6 +28,7 @@ function playersGuessSubmission(){
 	document.getElementById('guess').value = "";
 	checkGuess();
 }
+console.log('playersGuessSubmission defined')
 
 // Determine if the next guess should be a lower or higher number
 function lowerOrHigher(){
@@ -42,25 +44,24 @@ function guessMessage(){
 	var distance = Math.abs(winningNumber-playersGuess);
 	if (distance > 50) {
 		$('#info').html("Your guess is super cold (more than 50 digits away)! " + lowerOrHigher());
-		$('body').css('background', 'url(cold.gif)');
-		$('.main').css('background-color', 'blue');
+		$('body').css('background', 'url(images/cold.gif)');
+		$('.container').css('background-color', 'blue');
 	} else if (distance > 25 && distance <= 50) {
 		$('#info').html("Your guess is cold (more than 25 digits away). " + lowerOrHigher());
-		$('body').css('background', 'url(cool.gif)');
-		$('.main').css('background-color', 'lightblue');
+		$('body').css('background', 'url(images/cool.gif)');
+		$('.container').css('background-color', 'lightblue');
 	} else if (distance > 10 && distance <= 25) {
 		$('#info').html("Your guess is warm (within 25 units)! " + lowerOrHigher());
-		$('body').css('background', 'url(warm.gif)');
-		$('.main').css('background-color', 'yellow');
+		$('body').css('background', 'url(images/warm.gif)');
+		$('.container').css('background-color', 'yellow');
 	} else {
 		$('#info').html("Your guess is hot (within 10 units)! " + lowerOrHigher());
-		$('body').css('background', 'url(hot.gif)');
-		$('.main').css('background-color', 'red');
+		$('body').css('background', 'url(images/hot.gif)');
+		$('.container').css('background-color', 'red');
 	}	
 }
 
 // Check if the Player's Guess is the winning number 
-//don't guesses and count need to be global so they don't reset every single time function is called?
 var guesses =[];
 var count = 5;
 function checkGuess(){
@@ -70,8 +71,8 @@ function checkGuess(){
 				} else if (playersGuess === winningNumber){
 					$('#info').css('font-size', '30px');
 					$('#info').html(winningNumber + " is the correct number! You won!");
-					$('body').css('background', 'url(celebrate.gif)');
-					$('.main').css('background-color', 'rgb(248,248,255)');
+					setInterval(nextBackground, 1000);
+					$('.container').css('background-color', 'rgb(248,248,255)');
 				}else if(guesses.indexOf(playersGuess) > -1){
 					$('#info').html('You already guessed ' + playersGuess + '. Submit a new guess!');
 				}else {
@@ -81,8 +82,8 @@ function checkGuess(){
 		}
 	} else{
 		$('#info').html('Sorry, you ran out of turns :(. The correct number was ' + winningNumber + '. Please play again.');
-		$('body').css('background', 'url(loser.gif)');
-		$('.main').css('background-color', 'gray');
+		$('body').css('background', 'url(images/loser.gif)');
+		$('.container').css('background-color', 'gray');
 	}
 }
 
@@ -95,7 +96,7 @@ function checkGuess(){
 function provideHint(){
 	//count === 5 doesn't work!
 	if (count === 5){
-		$('info').html('Guess a number before you ask for a hint!');
+		$('#info').html('Guess a number before you ask for a hint!');
 	} else if (count === 4){
 		$('#info').html('One of the following is the winning number: ' + listArray());
 	} else if (count === 3){
@@ -125,9 +126,8 @@ function isPrime(){
 	}
 	return 'prime';
 }
-
+var arr = [];
 function listArray(){
-	var arr = [];
 	var listLength = count * 2;
 	for(var i = 0; i < listLength; i++){
 		arr.push(generateWinningNumber());
@@ -137,6 +137,7 @@ function listArray(){
 	return arr;
 }
 //keep getting 'once is not a function' if listed as var once 
+
 function once(func){
 	var called;
 	var inner = function(){
@@ -153,14 +154,33 @@ function playAgain(){
 	window.location.reload();
 }
 
+var backgrounds = new Array(
+    'url(images/celebrate.gif)'
+  , 'url(images/balloons.png)'
+);
 
-/* **** Event Listeners/Handlers ****  */
 
-//should i wrap everything around document.ready or just things that require jQuery?
+var current = 0;
+
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    $('body').css('background', backgrounds[current]);
+}
+
 $(document).ready(function(){
 	$(document).keydown(function(e){
 		if (e.keyCode === 13){
 			playersGuessSubmission();
 		}
 	});
-});
+	console.log('about to assign click handler');
+	$('#submit').on('click', playersGuessSubmission);
+	$('#hint').on('click', provideHint);
+	$('#again').on('click', playAgain);
+})
+})();
+
+/* **** Event Listeners/Handlers ****  */
+
+//should i wrap everything around document.ready or just things that require jQuery?
